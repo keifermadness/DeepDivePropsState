0dcbb38d04b4578a61e60a21393474d7ac1a3f5e
keifermadness <keifergilbert@gmail.com>
2021-07-14 09:45:51 -0700

    added toggle buttons and state logic to post buttons

--- src/components/journals/journal_list.js
+++ src/components/journals/journal_list.js
@@ -19,15 +19,34 @@ export default class JournalList extends Component {
             greeting: 'Hi there',
             isOpen: true 
         };
+    }
+
+    clearEntries() {
+        this.setState({ journalData: [], isOpen: false });
     };
 
+    showAllEntries() {
+        this.setState({ journalData: rawJournalData, isOpen: true});
+    }
+
   render() {
+    const journalEntries = this.state.journalData.map(journalEntry => {
+        return (
+            <div key={journalEntry.title}>
+                <JournalEntry title={journalEntry.title} 
+                content={journalEntry.content} 
+                />
+            </div>
+        );
+    });
+
     return( 
         <div>
             <h2>{ this.props.heading }</h2>
-            <JournalEntry title="some title" content="some content" />
-            <JournalEntry title="some other post" content="some content" />
-            <JournalEntry title="anotha one" content="some content" />
+            {journalEntries}
+
+            <button onClick={() => this.clearEntries()}>Clear Journal Entries</button>
+            <button onClick={() => this.showAllEntries()}>Show All Entries</button>
         </div>
     );
   }
